adae26f3c9ef4fd0bd0cc8e0efd45be238149674
Jose Eurico <140893366+josdecarv@users.noreply.github.com>
2023-10-20 02:51:01 +0100

    **AUTOMAÇÃO DE TAREFAS COM PYTHON – PyAutoGUI**
    **O QUE É A BIBLIOTECA PYAUTOGUI?**
    **A biblioteca PyAutoGUI**
    
    **A biblioteca PyAutoGUI** existe com uma função muito importante: **automatizar funções básicas de teclado e mouse em uma aplicação**. Dessa forma, permite fazer testes com maior precisão, de modo a **agilizar o processo de desenvolvimento**. Ou seja, fornece suporte multiplataforma para **gerenciar operações de mouse e teclado** por meio de código para permitir a automação de tarefas.
    
    Uma **ferramenta** como essa tem muitos aplicativos, alguns dos quais incluem tirar **capturas de tela, automatizar testes de GUI** (como Selenium), **automatizar tarefas que só podem ser feitas com uma GUI, etc.**
    
    **A biblioteca PyAutoGUI** no **Python** é uma mão na roda em muitos casos. Permite construir testes precisos e práticos para evoluir nos resultados no **desenvolvimento de software**.
    Com ela, os programadores perdem menos tempo e focam em aspectos mais importantes.
    
    **APLICAÇÕES PYAUTOGUI**
    Essa **biblioteca** é muito fácil de usar. Imagine que você precisa de testes para verificar a **consistência de uma aplicação**. Esses testes incluem, naturalmente, **funções básicas, como escrever e clicar, com o teclado e com mouse**.
    É preciso conduzir vários desses **testes**, incansavelmente, para garantir que o **software** está bom.
    
    Em algumas situações, você faz mudanças e precisa voltar e repetir a mesma sequência várias vezes, com poucas variações.
    
    Não seria incrível simplesmente ter como automatizar essa sequência de testes?
    Dessa maneira, você garantiria menos perda de tempo com essas funções e poderia focar em outros aspectos. Ou simplesmente poderia descançar um pouco.
    
    É para isso que serve o **PyAutoGUI**. A ideia é justamente tornar essas tarefas mais simples e automáticas, de modo a facilitar os testes. É possível fazer **cliques, mover mouse, escrever na tela e cumprir uma série de ações**.
    
    Com isso, o desenvolvedor consegue executar vários testes em bloco para assegurar a consistência de sua aplicação. É viável desenvolver uma lógica de repetição e condicional para os testes, seguindo o mesmo princípio normal da programação.
    
    Meu GitHub:
    github.com/josdecarv
    
    Algumas fontes consultadas:
    jobu.com.br/2021/08/03/pyautogui
    www.hastagtreinamentos.com/automacao-programa-sistema-python

---
+++ AutomacaoComPyAutoGUI.py
@@ -0,0 +1,29 @@
+import pyautogui
+import time
+
+pyautogui.PAUSE = 1
+
+pyautogui.alert("O nosso algoritmo vai rodar, pressiona OK, para começar!")
+
+# faça um screenshot e abre o paint
+pyautogui.press('printscreen')
+pyautogui.press('winleft')
+pyautogui.write('paint')
+pyautogui.press('enter')
+time.sleep(1)
+
+# dê clique e cole a imagen
+pyautogui.click()
+pyautogui.hotkey('ctrl', 'v')
+time.sleep(1)
+
+# salve a imagen e feche o programa
+pyautogui.hotkey('alt','f4')
+time.sleep(1)
+pyautogui.press('enter')
+pyautogui.write('screen')
+pyautogui.press('enter')
+time.sleep(1)
+
+# avise ao terminar as tarefas
+pyautogui.alert("Acabamos de executar o nosso algoritmo, pressione OK, para terminar.")
